ceae4836dd2874771abae90075faefc75565694f
Felipe Lima <felipe.lima@FLEX.LOCAL>
2023-10-26 15:01:04 -0300

    Styles: configurando a tela Home

--- src/components/Presentation/index.tsx
+++ src/components/Presentation/index.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import Styles from './Presentation.module.scss';
 
 export default function Presentation() {
+  const tela = screen.width;
+  console.log(tela);
   return (
     <div className={Styles.container}>
+      <div className={Styles.container__banner}>
+        dfsdsfds
 
+      </div>
     </div>
   );
 }
+
