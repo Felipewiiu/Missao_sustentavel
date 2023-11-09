248738dcbee7672466545be61a0eea9d5dafedfa
Felipe Lima <felipe.lima@FLEX.LOCAL>
2023-11-09 14:22:18 -0300

    Styles: Criando a animação de transição do dialogo

--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import {useState} from 'react';
 import Styles from './Presentation.module.scss';
 import { ReactComponent as Trash } from '../../assets/elements.svg';
 import planet from '../../assets/planeta 2.png';
 import robo from '../../assets/robo.svg';
 import { text } from '../../data/text';
 import Next from '../../assets/Next.png';
+import { useNavigate } from 'react-router-dom';
+
+
 
 export default function Presentation() {
-  const tela = screen.width;
-  console.log(tela);
+  const [count, setCount] = useState(0);
+  const navigate = useNavigate();
+
+
+  const changeText = () => {
+    setCount(count + 1);
+    console.log(count);
+    if (count >= 1) navigate('/');
+  };
+
   return (
     <div className={Styles.container}>
       <div className={Styles.container__banner}>
@@ -22,12 +33,12 @@ export default function Presentation() {
           <img src={robo} alt="Robo" className={Styles.robo} />
           <div className={Styles.container__text}>
             <p>
-              {text[1]}
+              {text[count]}
             </p>
-            <img src={Next} alt="Próximo"  className={Styles.next}/>
+            <img src={Next} alt="Próximo" className={Styles.next} onClick={changeText} />
           </div>
 
-          
+
         </div>
 
 
