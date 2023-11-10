1bf648fd97343e0cf171f205112bde255ee23515
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2023-11-09 22:35:38 -0300

    feat: implementando a trilha sonora

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Styles from './Home.module.scss';
 import { useNavigate } from 'react-router-dom';
+import audio1 from '../../assets/audio/soundOpen.mp3';
 
 export default function Home() {
   const navigate = useNavigate();
 
+  const openMusic = () => {
+    navigate('/presentation');
+    new Audio(audio1).play();
+  };
+
   return (
-    <div className={Styles.container} onClick={() => navigate('/presentation')}>
+    <div className={Styles.container} onClick={openMusic}>
       <button className={Styles.btn} >
         <p>
           Start Game
